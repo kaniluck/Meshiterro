f67b63c460a1596302a8555aa145e536a3a66e70
keisuketakahashi514 <takahashikeisuke.0514@gmail.com>
2021-03-09 02:48:17 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2685ae2c229ca7130daf8768a3926003269b1c80e302b9b6f570df3f1adeb932fde1d282539d7bf0e23a9f3ad109d9749f58de8ab526a471d4990e5a8b15f83f'
